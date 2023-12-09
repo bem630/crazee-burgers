29e6702346d60344932fc3a37c4be79e13ef1657
bem630 <bah70495@gmail.com>
2023-12-09 09:36:04 +0100

    ♻️ F01 : LOGIN > LoginPage > LoginForm > Add h1 and form in my component LoginForm

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,33 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+        // state
+        const [ inputValues, setInputValue ] = useState("");
+
+        // comportements
+        const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Bonjour ${inputValues}`)
+        setInputValue("");
+        }
+        const handleChange = (e) => {
+        setInputValue(e.target.value);
+        }
+        
+        
+
+        // affichage (render)
+
+        // création de formulaire 
+        // soumission du formalaire
+        //collecte de données
     return ( 
-        <div>LoginForm</div>
+        <form action="" onSubmit={handleSubmit}>
+            <h1>Bienvenue chez nous !</h1><br/>
+            <h2>Connectez vous</h2>
+            <input value={inputValues} onChange={handleChange} type="text" placeholder="Entrez votre prénom..." required />
+            <button>Accédez à votre espace</button>
+        </form>
      );
 }
  
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,36 +1,9 @@
-import { useState } from "react";
 import LoginForm from "./LoginForm";
 
 const LoginPage = () => {
-    // state
-    const [ inputValues, setInputValue ] = useState("");
-
-    // comportements
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      alert(`Bonjour ${inputValues}`)
-      setInputValue("");
-    }
-    const handleChange = (e) => {
-      setInputValue(e.target.value);
-    }
     
-    
-
-    // affichage (render)
-
-    // création de formulaire 
-    // soumission du formalaire
-    //collecte de données
     return ( 
         <div>
-            <h1>Bienvenue chez nous !</h1><br/>
-            <h2>Connectez vous</h2>
-            <form action="" onSubmit={handleSubmit}>
-                <input value={inputValues} onChange={handleChange}
-                 type="text" placeholder="Entrez votre prénom..." required />
-                <button>Accédez à votre espace</button>
-            </form>
             <LoginForm/>
         </div>
      );
