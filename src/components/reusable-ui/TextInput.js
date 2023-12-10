772d126956ed605790fa84d8acdb31538041e3d5
bem630 <bah70495@gmail.com>
2023-12-10 22:03:02 +0100

    ♻️ F03: TextInput > refactor this component

--- src/components/reusable-ui/TextInput.js
+++ src/components/reusable-ui/TextInput.js
@@ -1,7 +1,40 @@
-const TextInput = () => {
+import styled from "styled-components";
+
+const TextInput = ({value, onChange, icon, ...extraProps}) => {
     return ( 
-        <div>TextInput</div>
+        <TextInputStyled>
+            { icon && icon}
+            <input value={value} onChange={onChange} type="text" {...extraProps}  />
+        </TextInputStyled>
      );
 }
  
 export default TextInput;
+
+const TextInputStyled = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 18px 24px;
+    align-items: center;
+    background-color: white;
+    border-radius: 5px;
+    margin: 18px 0;
+    
+    .icon {
+    margin-right: 8px;
+    font-size: 15px;
+    color: #93a2b1;
+  }
+    input {
+        outline-color: blue;
+        border: none;
+        color: #17161a;
+        font-size: 15px;
+        width: 100%;
+        &::placeholder {
+    background-color: white;
+    color: lightgrey;
+  }
+    }
+    
+`;
