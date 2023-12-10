3329a133ef6e41e9631b8c5638645eb35a7a0e04
bem630 <bah70495@gmail.com>
2023-12-10 18:33:33 +0100

    💄 F03: LOGIN > Logo > Add styling in this component

--- src/components/reusable-ui/Logo.js
+++ src/components/reusable-ui/Logo.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 const Logo = () => {
     return ( 
         <LogoStyled>
-            Logo
+            <h1>CRAZYY</h1>
+                <img src="/images/logo-orange.png" alt="" />
+            <h1>BURGER</h1>
         </LogoStyled>
      );
 }
@@ -11,5 +13,25 @@ const Logo = () => {
 export default Logo;
 
 const LogoStyled = styled.div`
-  background-color: blue;
+    display: flex;
+    align-items: center;
+    transform: scale(2.5);
+
+  h1 {
+        display: inline;
+        text-align: center;
+        color: #ffa01b;
+        font-size: 36px;
+        font-weight: 700;
+        line-height: 1em;
+        letter-spacing: 1.5px;
+        font-family: "Amatic SC", cursive;
+    }
+  img {
+        height: 60px;
+        width: 80px;
+        margin: 0 5px;
+        object-fit: contain;
+        object-position: center;
+  }
 `;
