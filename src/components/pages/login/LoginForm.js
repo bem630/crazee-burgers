5bfca6e125012b6efe9a073355e6a390eba0d308
bem630 <bah70495@gmail.com>
2023-12-10 18:31:51 +0100

    💄 F03: LOGIN > LoginForm > Add styling in this component

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -28,10 +28,10 @@ const LoginForm = () => {
     return ( 
         <LoginFormStyled action="" onSubmit={handleSubmit}>
             <div>
-                <h1>Bienvenue chez nous !</h1><br/>
+                <h1>Bienvenue chez nous !</h1><hr/>
                 <h2>Connectez vous</h2>
             </div>
-            <div>
+            <div className="form-gauche">
                 <input value={inputValues} onChange={handleChange} type="text" placeholder="Entrez votre prénom" required />
                 <button>Accédez à mon espace</button>
             </div>
@@ -42,11 +42,49 @@ const LoginForm = () => {
 export default LoginForm;
 
 const LoginFormStyled = styled.form`
-  text-align: center;
-  background-color: green;
+    font-family: 'Amatic SC', cursive;
+    border-radius: 5px;
+    text-align: center;
+    max-width: 500px;
+    min-width: 400px;
+    background-color: green;
+    margin: 0 auto;
+    padding: 1.5rem 1rem;
+  /*
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  font-family: 'Open Sans', sans-serif;
+  */
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+  hr {
+    border: 1.5px solid #f56a2c;
+    //width: 100%;
+    margin-bottom: 40px;
+  }
+  h2 {
+    color: white;
+    font-size: 36px;
+    margin: 20px 10px;
+  }
+  .form-gauche {
+    display: flex;
+    flex-direction: column;
+    //align-items: center;
+    justify-content: center;
+    input {
+        margin-bottom: 10px;
+        padding: 8px 20px;
+    }
+    button {
+        margin-bottom: 10px;
+        padding: 8px 20px;
+        cursor: pointer;
+    }
+  }
 
 `;
