44e9601d790064419bdfc365fcb10f254f8841b0
bem630 <bah70495@gmail.com>
2023-12-13 13:29:13 +0100

    🚧 F05: ORDER > MAIN > Add grid layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
 
+
 const Main = () => {
+    
     return ( 
-        <MainStyled>Main</MainStyled>
+        <MainStyled>
+            <div className="basket">Basket</div>
+            <div className="menu">Menu</div>
+        </MainStyled>
      );
 }
  
@@ -16,4 +21,65 @@ const MainStyled = styled.div`
         border-bottom-right-radius: ${theme.borderRadius.extraRound};
         border-bottom-left-radius: ${theme.borderRadius.extraRound};
         box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+        display: grid;
+        grid-template-columns: 25% 1fr;
+
+        .basket {
+            background: blue;
+        }
+        .menu {
+            background: yellow;
+        }
+        
+
+        /*!SECTION
+        <div className="cards" >
+                {fakeMenu1.map((data) => {
+                    <div className="card" key={data.id}>
+                    <div>{data.imageSource}</div>
+                    <div className="card-content">
+                        <h1>{data.title}</h1>
+                        <div className="card-footer">
+                            <div className="price">{data.title}</div>
+                            <button className="button">Ajouter</button>
+                        </div>
+                    </div>
+                </div>
+                } )}
+                
+            </div>
+
+        .cards {
+            display: flex;
+            justify-content: space-evenly;
+            
+            .card {
+                background: lightgrey;
+                width: 240px;
+                height: 330px;
+                margin-right: 20px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                
+                img {
+                    width: 240px;
+                    height: 145px;
+                }
+                .card-content {
+                    
+                    h1 {
+                        text-transform: uppercase;
+                        font-size: 18px;
+                        padding-left: 20px;
+                    }
+                    .card-footer {
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                }
+                }
+                
+            }
+        }*/
 `;
