9ee1a52e585a0f99707d4ad036dfe355ac1e6fdc
bem630 <bah70495@gmail.com>
2023-12-15 01:18:42 +0100

    💄 F05: ORDER > MENU > Product > Add final style

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu"
 import { theme } from "../../../../theme";
+import Product from "./Product";
 
 const Menu = () => {
     const [menu, setMenu] = useState(fakeMenu2);
@@ -9,18 +10,7 @@ const Menu = () => {
         <MenuStyled>
             {menu.map((produit) => {
                 return (
-                    <div className="produit">
-                        <div className="image">
-                            <img src={produit.imageSource} alt={produit.title} />
-                        </div>
-                        <div className="info-text">
-                            <div className="title">{produit.title}</div>
-                            <div className="description">
-                                <div className="price">{produit.price}</div>
-                                <button className="add-button">Ajouter</button>
-                            </div>
-                        </div>
-                    </div>
+                    <Product imageSource ={produit.imageSource} title={produit.title} price={produit.price} />
                 )
                 } )}
         </MenuStyled>
@@ -38,20 +28,5 @@ const MenuStyled = styled.div`
     padding: 50px 50px 150px;
     justify-items: center;
     
-
-    .produit {
-        background: red;
-        width: 240px;
-        height: 330px;
-        //box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 20%);
-        .image {
-            width: 240px;
-            height: 145px;
-            img {
-            width: 100%;
-            height: 100%;
-        }
-        }
-    }
         
 `;
