56e609512a37e1afb8c6011162a302a5b246f2c1
bem630 <bah70495@gmail.com>
2023-12-17 17:16:04 +0100

    💄 F07: ADMIN > Add AdminTabs and AdminPanel styling

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
+import Tab from "../../../../reusable-ui/Tab";
+import { FiChevronDown } from "react-icons/fi";
 
 const AdminTabs = () => {
     return ( 
-        <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+        <AdminTabsStyled>
+            <Tab Icon = {<FiChevronDown className="btnChevronDown"/>} />
+        </AdminTabsStyled>
      );
 }
  
@@ -11,5 +15,6 @@ export default AdminTabs;
 
 const AdminTabsStyled = styled.div`
     background-color: ${theme.colors.red};
-        
+    display: flex;
+    padding: 0 20px;
 `;
