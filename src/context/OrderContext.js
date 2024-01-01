6deae0923e5f0dd4b1cd07ede5bbed846a23163c
bem630 <bah70495@gmail.com>
2024-01-01 12:18:33 +0100

    🚧 F08: ADMIN > AddForm > Delete to setMenu to orderContext

--- src/context/OrderContext.js
+++ src/context/OrderContext.js
@@ -11,6 +11,6 @@ export default createContext({
     currentTabSelected: () => {},
     setcurrentTabSelected: () => {},
     menu: [],
-    setMenu: () => {},
+    
     handleAddProduit: () => {},
 })
