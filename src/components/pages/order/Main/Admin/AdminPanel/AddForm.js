b7bde6f7d418388b52013b7921da9529fb25e727
bem630 <bah70495@gmail.com>
2024-01-23 04:56:22 +0100

    ♻️  F08: ADMIN > Add preview-image (style)

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
@@ -1,5 +1,5 @@
-import { FaHamburger } from "react-icons/fa";
-import { BsFillCameraFill } from "react-icons/bs";
+//import { FaHamburger } from "react-icons/fa";
+//import { BsFillCameraFill } from "react-icons/bs";
 //import { MdOutlineEuro } from "react-icons/md";
 import { FiCheck } from "react-icons/fi";
 import styled from "styled-components";
@@ -70,7 +70,7 @@ const AddForm = () => {
                     {newProduct.imageSource ? (
                         <img src={newProduct.imageSource} alt={newProduct.title} />
                         ) : (
-                            <div>Aucune image</div>
+                            <div className="empty-image">Aucune image</div>
                             )}
                 </div>
                 <div className="input-fields">
@@ -118,14 +118,15 @@ const AddForm = () => {
 export default AddForm;
 
 const AddFormStyled = styled.form`
-    border: 2px solid black;
+    //border: 2px solid black;
     display: grid;
     height: 100%;
     width: 70%;
     grid-template-columns: 1fr 3fr;
     grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
     .image-preview {
-        background: red;
         grid-area: 1 / 1 / 4 / 2;
         display: flex;
         justify-content: center;
@@ -136,6 +137,18 @@ const AddFormStyled = styled.form`
             height: 100%;
             object-fit: contain;
             object-position: center;
+        }
+
+        .empty-image {
+            height: 100%;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid ${theme.colors.greyLight};
+            line-height: 1.5;
+            color: ${theme.colors.greySemiDark};
+            border-radius: ${theme.borderRadius.round};
         }
     }
     .input-fields {
