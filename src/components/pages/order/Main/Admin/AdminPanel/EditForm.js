e9cc7776c884e7e3fbd5ab28a3a8569ff78d9ff5
bem630 <bah70495@gmail.com>
2024-01-30 14:34:13 +0100

    🚧 F09: ADMIN >EDITFORM > Retrieve into of Card Clicked on with form from ADFORM

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.js
@@ -1,15 +1,67 @@
-import { useContext } from "react";
-import HintMessage from "./HintMessage";
+import { useContext, useState } from "react";
+//import HintMessage from "./HintMessage";
 import OrderContext from "../../../../../../context/OrderContext";
+import styled from "styled-components";
+import ImagePreview from "./ImagePreview";
+import TextInput from "../../../../../reusable-ui/TextInput";
+import { getInputTextsConfig } from "./InputTextConfig";
+import { EMPTY_PRODUCT } from "../../../../../../enums/product";
 
 const EditForm = () => {
     const { productSelected } = useContext(OrderContext);
+    const [productBeingEdit, setProductBeingEdit] = useState(EMPTY_PRODUCT)
+
+    
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setProductBeingEdit({...productBeingEdit, [name]: value })
+    }
+    const inputTexts = getInputTextsConfig(productSelected);
     return ( 
-        <div>
-            <HintMessage/>
-            <span>{productSelected && productSelected.title}</span>
-        </div>
+        <EditFormStyled>
+                <ImagePreview imageSource={productSelected.imageSource} title={productSelected.title}/>
+                <div className="input-fields">
+                        {inputTexts.map (
+                            (input) => (
+                                <TextInput {...input} key={input.id} onChange={handleChange} version="minimalist"/>
+                            )
+                        ) }
+                    
+                </div>
+            </EditFormStyled>
      );
 }
  
 export default EditForm;
+
+const EditFormStyled = styled.div`
+  display: grid;
+    height: 100%;
+    width: 70%;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
+
+    
+    .input-fields {
+        grid-area: 1 / 2 / -2 / 3;
+        display: grid;
+        grid-row-gap: 8px;
+    }
+    .submit {
+        grid-area: 4 / -2 / -1 / -1;
+        display:flex;
+        align-items: center;
+        position: relative;
+        top: 3px;
+
+        .submit-button {
+            //width: 50%;
+            height: 100%;
+        }
+
+
+    }  
+`;
